6bc401bc9e15d972292d256412b6ff2688e7111b
Randolph Pe Benito <randolph@xy-randolph.xyber.ph>
2016-04-26 10:07:35 +0800

    Added publish and draft.

--- webapp/posts/models.py
+++ webapp/posts/models.py
@@ -4,10 +4,20 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils import timezone
 
 from django.utils.text import slugify
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
+
+
+#Post.objects.all()
+#Post.objects.create(user=user, title="Some time")
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        # Post.objects.all() = super(PostManager, self).all()
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 
 def upload_location(instance, filename):
@@ -27,8 +37,12 @@ class Post(models.Model):
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
@@ -63,5 +77,6 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
+
 
 
